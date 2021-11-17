550114f3b346a07aef64e00f6ee8e36e97b68785
vijayprabhu04 <49646334+vijayprabhu04@users.noreply.github.com>
2021-11-17 10:12:07 +0530

    Add files via upload

---
+++ terraform/4-variables/E4_with_variables/variable.tf
@@ -0,0 +1,14 @@
+# # Access Key
+variable AWS_ACCESSKEY {
+  default     = "AKIA5YGJ6CXR3AJLN7WO"
+}
+
+# Secret Key
+variable AWS_SECRETKEY {
+  default     = "TMDkmoNZl5WelsMNTC3V0tHEdffTpo7Fv5L7GiGu"
+}
+
+# Region
+variable AWS_REGION {
+  default     = "ap-south-1"
+}
---
+++ terraform/4-variables/E5_cmd_line_input/vars.tf
@@ -0,0 +1,22 @@
+# Access Key
+variable AWS_ACCESSKEY {
+  default     = "AKIA5YGJ6CXR3AJLN7WO"
+}
+
+# Secret Key
+variable AWS_SECRETKEY {
+  default     = "TMDkmoNZl5WelsMNTC3V0tHEdffTpo7Fv5L7GiGu"
+}
+
+# Region
+variable AWS_REGION {
+  default = "ap-south-1"
+}
+
+variable machine_type {
+  default = "t2.micro"
+}
+
+# Image IDs
+variable "AMI" {}
+
