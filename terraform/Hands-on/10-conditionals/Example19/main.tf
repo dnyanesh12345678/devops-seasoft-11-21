0787212967d0199d001d6bb82810bb8791dc568c
vijayprabhu04 <49646334+vijayprabhu04@users.noreply.github.com>
2021-12-08 08:59:32 +0530

    Delete terraform/Hands-on/10-conditionals/Example19 directory

--- terraform/Hands-on/10-conditionals/Example19/main.tf
+++
@@ -1,9 +0,0 @@
-# Create incremental instance:
-resource "aws_instance" "apache_instance" {
-  count         = var.instance_data.count
-  ami           = var.instance_data.ami
-  instance_type = var.instance_data.type
-  tags = {
-    Name  = "web_server_${count.index}"
-  }
-}
